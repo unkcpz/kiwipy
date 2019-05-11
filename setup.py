9389efa33f892fd4abd98a6b5a663c1020fef7e2
Jason Eu <morty.yu@yahoo.com>
2019-05-11 12:46:48 +0800

    en 2019-5-11

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
 from setuptools import setup
 
+# origin author
 __author__ = "Martin Uhrin"
 __license__ = "GPLv3 and MIT, see LICENSE file"
 __contributors__ = "Sebastiaan Huber"
+# modified
+__author__ = "Jason Eu"
+__modifier__ = "Jason Eu"
 
 about = {}
 with open('kiwipy/version.py') as f:
@@ -15,36 +18,27 @@ setup(
     version=about['__version__'],
     description='A python remote communications library',
     long_description=open('README.rst').read(),
-    url='https://github.com/muhrin/kiwipy.git',
-    author='Martin Uhrin',
-    author_email='martin.uhrin@gmail.com',
+    url='https://github.com/unkcpz/kiwipy.git',
+    author='Jason Eu',
+    author_email='morty.yu@yahoo.com',
     license=__license__,
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='communication messaging rpc broadcast',
     install_requires=[
-        'six',
         'shortuuid',
-        'typing; python_version<"3.5"',
-        'enum34; python_version<"3.4"',
-        'backports.tempfile; python_version<"3.2"',
-        'futures; python_version == "2.7"',
     ],
     extras_require={
         'rmq': [
             'pika>=1.0.0b2',
-            'topika>=0.2.0, <0.3.0',
-            'tornado>=4; python_version<"3"',
-            'tornado<5; python_version>="3"',
-            'pyyaml'
+            'aio_pika',
+            'tornado>5',
+            'pyyaml>=3.13'
         ],
         'dev': [
             'pip',
@@ -56,8 +50,7 @@ setup(
             'twine',
             'yapf',
             'prospector',
-            'pylint<2; python_version<"3"',
-            'pylint; python_version>="3"',
+            'pylint',
         ],
     },
     packages=['kiwipy', 'kiwipy.rmq'],
